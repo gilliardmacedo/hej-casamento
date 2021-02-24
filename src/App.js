024d7442c6945be30363b151c98285b4c44176c2
Gilliard Macedo <gilliardmacedo@gmail.com>
2021-02-24 01:38:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 function App() {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  }
+
+  console.log(errors);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Ingresso</h1>
+      <p>Insira as informações para gerar os seus ingressos</p>
+      <div className="container"> 
+        <input type="tel" placeholder="Seu telefone (com DDD)" name="phone" ref={register({required: true, minLength:11, maxLength: 11})} />
+      </div>
+      <div className="container"> 
+        <input type="text" placeholder="Senha que você recebeu por mensagem" name="token" ref={register({required: true, minLength:5, maxLength: 5})} />
+      </div>
+      <input type="submit" value="Gerar"/>
+    </form>
   );
 }
 

3ef39c8eab685388676094ec39298a501e7f20c7
Gilliard Macedo <gilliardmacedo@gmail.com>
2021-02-25 02:29:48 -0300

    remover basepath

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="hej-casamento">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
